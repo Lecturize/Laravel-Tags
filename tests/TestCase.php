8333da8f30e1adb11fd135a59591bf0aa72e1499
AlexanderPoellmann <alex@lecturize.com>
2020-11-14 15:25:38 +0100

    Add testing boilerplate

---
+++ tests/TestCase.php
@@ -0,0 +1,24 @@
+<?php namespace Lecturize\Tags\Tests;
+
+use Lecturize\Tags\TagsServiceProvider;
+
+class TestCase extends \Orchestra\Testbench\TestCase
+{
+    public function setUp(): void
+    {
+        parent::setUp();
+        // additional setup
+    }
+
+    protected function getPackageProviders($app)
+    {
+        return [
+            TagsServiceProvider::class,
+        ];
+    }
+
+    protected function getEnvironmentSetUp($app)
+    {
+        // perform environment setup
+    }
+}
